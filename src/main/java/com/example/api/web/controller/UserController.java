f705ae77322a1b6554e846e7d1d82049d92d967a
Derik Bortoletto <derikbortolettoti@gmail.com>
2023-01-24 21:50:33 -0300

    Added UserDTO to return data

--- src/main/java/com/example/api/web/controller/UserController.java
+++ src/main/java/com/example/api/web/controller/UserController.java
@@ -1,16 +1,15 @@
 package com.example.api.web.controller;
 
+import com.example.api.web.dto.UserDTO;
 import com.example.api.persistence.entities.User;
 import com.example.api.services.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -23,9 +22,10 @@ public class UserController {
     }
 
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
 
         List<User> list = service.findAll();
-        return ResponseEntity.ok().body(list);
+        List<UserDTO> listDto = list.stream().map(UserDTO::new).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
     }
 }
---
+++ src/main/java/com/example/api/web/dto/UserDTO.java
@@ -0,0 +1,29 @@
+package com.example.api.web.dto;
+
+import com.example.api.persistence.entities.User;
+import lombok.*;
+
+import java.io.Serializable;
+
+public class UserDTO implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Getter
+    @Setter
+    private String id;
+
+    @Getter
+    @Setter
+    private String name;
+
+    @Getter
+    @Setter
+    private String email;
+
+    public UserDTO(User user) {
+        this.id = user.getId();
+        this.name = user.getName();
+        this.email = user.getEmail();
+    }
+
+}
